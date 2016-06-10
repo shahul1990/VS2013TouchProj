8476c45d040513858321356382c05b300f308fc1
shahul1990@gmail.com <shahul1990@gmail.com>
2016-06-10 09:15:16 +0530

    first commit

---
+++ Restaurant_Amb_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Restaurant_Amb_1.MvcApplication" Language="C#" %>
---
+++ Restaurant_Amb_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Restaurant_Amb_1.Startup))]
+namespace Restaurant_Amb_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
