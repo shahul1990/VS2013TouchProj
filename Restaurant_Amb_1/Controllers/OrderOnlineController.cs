a5765de6cf4277b77c1802851dc9e24c7f179aca
shahul1990@gmail.com <shahul1990@gmail.com>
2016-06-13 23:37:46 +0530

    2nd commit

---
+++ Restaurant_Amb_1/Controllers/OrderOnlineController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant_Amb_1.Views.OrderOnline
+{
+    public class OrderOnlineController : Controller
+    {
+        //
+        // GET: /OrderOnline/
+        public ActionResult area()
+        {
+            return View();
+        }
+	}
+}
